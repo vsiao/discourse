15e72dc6ac35c09139a7bc3a3556aa623b3686ee
Jason MacDonald <jason@cmu-350744.wv.cc.cmu.edu>
2012-02-11 16:35:03 -0500

    images and latex rendering

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,16 @@
 class Message < ActiveRecord::Base
   attr_accessible :content
+  
+  def self.convert_to_latex(string)
+    latex = ""
+    string.split("$").each_with_index do |x, i|
+      if (i % 2) == 0
+        latex += x
+      else
+        latex += '<img src="http://latex.codecogs.com/svg.latex?' + x.gsub(/ /,'%20') + '" />'
+      end
+    end
+    
+    return latex
+  end
 end
