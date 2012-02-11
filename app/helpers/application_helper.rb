b38a69dfa4d4f8f5c7876b696093f05078c69de6
Jason MacDonald <jason@cmu-350744.wv.cc.cmu.edu>
2012-02-11 01:07:29 -0500

    Using Jason's IP for faye server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://128.237.116.88:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
+  var faye = new Faye.Client('http://128.237.116.88:9292/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
