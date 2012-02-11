c3a42df3db8b419f6f178b0a8b2c914d5faf33ba
Jason MacDonald <jason@cmu-350744.wv.cc.cmu.edu>
2012-02-11 00:43:25 -0500

    Another faye fix

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function() {
-  var faye = new Faye.Client('http://fayeredis.herokuapp.com/faye');
+  var faye = new Faye.Client('http://localhost:9292/faye');
   faye.subscribe("/messages/new", function(data) {
     eval(data);
   });
