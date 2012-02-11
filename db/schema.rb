2ec492908d9472a0f05e9d8824fdf8c26f7fe2c0
Vincent Siao <vincentsiao@gmail.com>
2012-02-11 11:07:02 -0500

    added description to channels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120211125423) do
+ActiveRecord::Schema.define(:version => 20120211160242) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120211125423) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "chats", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 cmu = School.create(:name => "Carnegie Mellon University")
 c1 = Channel.create(:name => "15-251", :school_id => cmu.id)
 c2 = Channel.create(:name => "random", :school_id => cmu.id)
+c1.update_attribute(:description, "This course is about how to use theoretical ideas to formulate and solve problems in computer science. It integrates mathematical material with general problem solving techniques and computer science applications. Examples are drawn from Algorithms, Complexity Theory, Game Theory, Probability Theory, Graph Theory, Automata Theory, Algebra, Cryptography, and Combinatorics. Assignments involve both mathematical proofs and programming. ")
